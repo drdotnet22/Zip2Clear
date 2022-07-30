86f17035878dc85749074d671d787cfd50950edc
wannabegeek-1 <88114694+wannabegeek-1@users.noreply.github.com>
2022-07-30 18:30:00 -0400

    Added ListState. Not sure what I'm doing.

---
+++ Data/ListState.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Zip2Clear.Data
+{
+    public class ListState
+    {
+        public IEnumerable<Declaration> declarations { get; set; }
+        public IEnumerable<Vendor> vendors { get; set; } 
+        public IEnumerable<Invoice> invoices { get; set; }
+        public IEnumerable<Item> items { get; set; }
+        public IEnumerable<Tariff> tariff { get; set; }
+        public IEnumerable<Uom> uom { get; set; }
+    }
+}
